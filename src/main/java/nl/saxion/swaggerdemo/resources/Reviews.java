523e9863393816feb9782f4a81521bea0fcdbb29
Marco jansen <jansen.marco95@gmail.com>
2016-06-08 11:36:13 +0200

    Last fixes

--- src/main/java/nl/saxion/swaggerdemo/resources/Reviews.java
+++ src/main/java/nl/saxion/swaggerdemo/resources/Reviews.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 /**
